36c823bb8b24b8c1a72b59c9d8468b24a5c3a583
AndrewBoshra <andrew.boshr@gmail.com>
2022-02-22 22:22:42 +0200

    small modifications

---
+++ lib/commands/download_file.dart
@@ -0,0 +1,9 @@
+import 'package:shair/commands/abstract_command.dart';
+
+class DownloadFileCommand extends ICommand {
+  @override
+  execute() {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+}
